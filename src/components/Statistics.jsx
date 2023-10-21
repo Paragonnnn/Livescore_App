eeb06a147dede5081faa52906cc21a06b0fbfa15
Oluwaseyi <hassanabdullahioluwaseyi@gmail.com>
2023-10-21 23:45:43 +0100

    update

--- src/components/Statistics.jsx
+++ src/components/Statistics.jsx
@@ -14,13 +14,13 @@ const Statistics = ({statToggle, stats}) => {
                       <div className='text-center text-gray-400 text-lg my-2   '>{s.type}</div>
                       <div className='flex items-center justify-center gap-2  w-full py-2'>
                         <div className='w-[45%] bg-customBg h-[6px] rounded-l-full overflow-hidden flex justify-end border border-solid border-customBg'>
-                        <div className={s.type === 'Ball Possession' ? `w-[${parseInt(s.home.slice(0,s.home.indexOf('%')))}%]` :`w-[${Math.round(((+s.home * 100)/(+s.home + +s.away)))}%] bg-[#042a2b] h-[6px]  `}></div>
+                        <div className={s.type === 'Ball Possession' ? `w-[${s.home}] bg-[#042a2b] h-[6px]` :`w-[${Math.round(((+s.home * 100)/(+s.home + +s.away)))}%] bg-[#042a2b] h-[6px]  `}></div>
 
                         </div>
                         <div className='flex  w-[100px] justify-center'>{s.home} - {s.away}</div>
                         <div className='w-[45%] bg-customBg h-[6px] overflow-hidden  rounded-r-full border border-solid border-customBg'>
 
-                        <div className={s.type === 'Ball Possession' ? `w-[${s.away}]` :`w-[${Math.round(((+s.away * 100)/(+s.home + +s.away)))}%]  bg-[#042a2b] h-[6px] `}></div>
+                        <div className={s.type === 'Ball Possession' ? `w-[${s.away}] bg-[#042a2b] h-[6px]` :`w-[${Math.round(((+s.away * 100)/(+s.home + +s.away)))}%]  bg-[#042a2b] h-[6px] `}></div>
                         </div>
                       </div>
                     
